6dc42a3ed269fe0eaaa48643c407786d74d28479
EmanuelEE17 <47884166+manfreden@users.noreply.github.com>
2019-09-04 13:27:40 +0200

    Add files via upload

---
+++ .py
@@ -0,0 +1,11 @@
+# sariadler för att komma ihåg namn och åöder
+
+# läsa in namn
+name = input("skriv in ditt namn:")
+# läsa in ålder
+age = int(input("hur gamal är du:"))
+
+# skriva ut hej namn och du är x gammal
+print("hejsan", name,", välkommen till mitt program")
+print("du ät", age, "gammal!")
+# skriva ut 
