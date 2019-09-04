07e22ad527b052bab58402038a42ad29e26cefdb
EmanuelEE17 <47884166+manfreden@users.noreply.github.com>
2019-09-04 13:27:20 +0200

    Add files via upload

---
+++ lek1.py
@@ -0,0 +1,20 @@
+firstName = "Emanuel"
+middleName = "Per, Göran, Manfred"
+lstName = "Andersson"
+
+NickName = "Sox"
+
+# för.efternam
+# emanuel.andersson
+# firsName[3].lstName[3]
+
+# Print(f"firstName[0:3].lower()+""+lstName[0:3].lower()19")
+userName = firstName[0:3].lower()
+userName += lstName[0:3].lower()
+userName += "19"
+print(userName)
+#print(lstName.UPPER())
+
+#tecken = """() parenteser,
+#[] Hakparanteser
+#{} må
